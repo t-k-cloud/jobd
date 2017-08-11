4a782bde4d8d7142125703f16057c7245e9d40dd
t-k- <clock126@126.com>
2017-08-11 11:32:39 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+# must be root permission
+touch /root/test || exit
+
+# install commands used through jobs
 pacman --noconfirm -S nodejs yarn npm lsof
+
+# create job directory under home
 ln -sf `pwd`/examples/jobs /home/tk/
+
+# install node modules for src
 pushd src
+yarn install
+popd
+
+# install node modules for tests
+pushd tests
 yarn install
 popd
