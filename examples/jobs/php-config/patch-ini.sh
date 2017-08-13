8f0029ae5a25c534b5afab8f5cc6ca52c3657819
t-k- <clock126@126.com>
2017-08-13 11:19:19 +0800

    increase file upload size in PHP

--- examples/jobs/php-config/patch-ini.sh
+++ examples/jobs/php-config/patch-ini.sh
@@ -3,6 +3,12 @@ F=$(php-fpm7.0 -i | grep  '^Loaded Config' | awk -F'>' '{gsub(/ /, "", $0); prin
 echo "Patching PHP.ini file: $F"
 
 ###
+# set upload size
+###
+sed -i '/post_max_size/c post_max_size=32M' $F
+sed -i '/upload_max_filesize/c upload_max_filesize=32M' $F
+
+###
 # change open_basedir and display_errors
 ###
 sed -i '/open_basedir.*=/c ;open_basedir =' $F
