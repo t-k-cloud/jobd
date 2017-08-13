6b6ce764be2d9b8fa67a902e7ff91b101b691293
t-k- <clock126@126.com>
2017-08-13 10:07:01 +0800

    decouple

--- examples/jobs/php-config/php-ini-patch.sh
+++ examples/jobs/php-config/php-ini-patch.sh
@@ -14,9 +14,3 @@ sed -i 's/;extension=mysql\.so/extension=mysql\.so/' $F
 sed -i 's/;extension=mysqli\.so/extension=mysqli\.so/' $F
 sed -i 's/;extension=gd\.so/extension=gd\.so/' $F
 sed -i 's/;extension=pdo_mysql\.so/extension=pdo_mysql\.so/' $F
-
-###
-# restart service
-###
-systemctl restart php-fpm nginx
-systemctl enable php-fpm nginx
