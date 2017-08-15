5d564abf33bbc4934ce6c3dd47a4d9f34dfbf4c2
t-k- <clock126@126.com>
2017-08-15 15:59:05 -0400

    minor change

--- examples/jobs/droppy-wstunnel-cli-setup.sh
+++ examples/jobs/droppy-wstunnel-cli-setup.sh
@@ -7,8 +7,7 @@ $curdir/gitrepo-mirror.sh \
 	https://github.com/t-k-cloud/wsproxy ~/wsproxy
 
 cd ~/wsproxy
-echo "start new instances ..."
-
+echo "start new instances, connecting: $REMOTE_IP"
 for i in `seq 1 4`; do
 	echo "instance $i ..."
 	node ./wsproxy-cli.js $REMOTE_IP > wsproxy-cli-${i}.log <&- 2>&1 &
