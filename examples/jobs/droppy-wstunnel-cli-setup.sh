d44c5e5076853e70b81286ca66ca64b432815774
t-k- <clock126@126.com>
2017-08-15 15:56:12 -0400

    wsproxy-cli passing remote IP argument.

--- examples/jobs/droppy-wstunnel-cli-setup.sh
+++ examples/jobs/droppy-wstunnel-cli-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 curdir=$(cd $(dirname $0) && pwd)
+source $curdir/env.cfg
+REMOTE_IP=$(echo $SSHTO | awk -F@ '{print $2}')
 
 $curdir/gitrepo-mirror.sh \
 	https://github.com/t-k-cloud/wsproxy ~/wsproxy
@@ -9,7 +11,7 @@ echo "start new instances ..."
 
 for i in `seq 1 4`; do
 	echo "instance $i ..."
-	node ./wsproxy-cli.js >wsproxy-cli-${i}.log <&- 2>&1 &
+	node ./wsproxy-cli.js $REMOTE_IP > wsproxy-cli-${i}.log <&- 2>&1 &
 	pid=$!
 	echo $pid > wsproxy-cli-${i}.pid
 done
