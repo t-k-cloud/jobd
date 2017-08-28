ef50683ab140c63db8d316dbb1e1dda89822cec8
t-k- <clock126@126.com>
2017-08-28 16:10:28 -0400

    cloud setup moved to master repo.

--- setup.sh
+++ setup.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-# must be root permission
-touch /root/test || exit
-
-# install commands used through jobs
-pacman --noconfirm -S nodejs yarn npm
-pacman --noconfirm -S lsof rsync
-pacman --noconfirm -S openssh # for ssh and scp
-
-# create job directory under home
-ln -sf `pwd`/examples/jobs /home/tk/
-
 # install node modules for src
 pushd src
 yarn install
